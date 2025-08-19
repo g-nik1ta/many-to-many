bbfef842184cbb9b81b8aa3aea0245a29a0183b1
Nikita <nikita.gonohov.it@gmail.com>
2025-08-19 22:08:11 +0300

    add TS

--- src/components/ItemsList/ItemsList.tsx
+++ src/components/ItemsList/ItemsList.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './ItemsList.scss';
 
 const ItemsList = () => {
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router";
 
-const root = document.getElementById("root");
+const root = document.getElementById("root") as HTMLElement;
 
 ReactDOM.createRoot(root).render(
     <BrowserRouter>
