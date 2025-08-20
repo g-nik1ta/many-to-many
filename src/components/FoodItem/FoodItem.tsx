5acb885765c3f10c2c5d1d60c2ec32dcbb8a18ef
Nikita <nikita.gonohov.it@gmail.com>
2025-08-20 12:23:00 +0300

    food item img lazy loading + skeleton

--- src/components/FoodItem/FoodItem.tsx
+++ src/components/FoodItem/FoodItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./FoodItem.scss";
 import { Food } from "../../types/item";
 
@@ -7,18 +7,31 @@ interface FoodItemProps {
 }
 
 const FoodItem: React.FC<FoodItemProps> = ({ item }) => {
+    const [visible, setVisible] = useState(false);
+    const [loadedImg, setLoadedImg] = useState(false);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => setVisible(true), 10);
+        return () => clearTimeout(timeout);
+    }, []);
+
     return (
         <div
-            className="food-item"
+            className={`food-item ${visible ? "visible" : ""}`}
             style={{ backgroundColor: `#${item.color}` }}
         >
             <span className="food-name">{item.name}</span>
             {item.image && process.env.REACT_APP_API_URL && (
-                <img
-                    className="food-image"
-                    src={process.env.REACT_APP_API_URL + item.image}
-                    alt={item.name + " image"}
-                />
+                <>
+                    {!loadedImg && <div className="skeleton" />}
+                    <img
+                        className="food-image"
+                        src={process.env.REACT_APP_API_URL + item.image}
+                        alt={item.name + " image"}
+                        loading="lazy"
+                        onLoad={() => setLoadedImg(true)}
+                    />
+                </>
             )}
         </div>
     );
