ca56dc23477a748de71211c181a80ffe282e9262
Nikita <nikita.gonohov.it@gmail.com>
2025-08-20 13:05:17 +0300

    hash router

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import { FoodProvider } from "./context/FoodContext";
 
 const root = document.getElementById("root") as HTMLElement;
 
 ReactDOM.createRoot(root).render(
-    <BrowserRouter>
+    <HashRouter>
         <FoodProvider>
             <App />
         </FoodProvider>
-    </BrowserRouter>
+    </HashRouter>
 );
